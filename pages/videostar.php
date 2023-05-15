3d415de72c9533f13bafc6cdb92ba3ea0a379035
HAMAEXPERT <77998844+hamacracker@users.noreply.github.com>
2023-05-15 14:09:53 +0300

    Add files via upload

--- pages/videostar.php
+++ pages/videostar.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>video star</title>
+  <title></title>
 </head>
 <body>
   <div class="page">
@@ -12,11 +12,11 @@
           "ios-only">Back</span></a>
         </div>
         <div class="title">
-          VideoStar++
+          Videostar
         </div>
         <div class="right">
           <a href="javascript:window.location.reload(true)" class=
-          "f7-icons">reload</a>
+          "f7-icons">bi bi-arrow-clockwise</a>
         </div>
       </div>
     </div>
@@ -26,20 +26,22 @@
           <li class="media-item">
             <div class="item-content">
               <div class="item-media"><img src=
-              "./img/videostar.png"
+              "./img/Videostar.png"
               class="appicon elevation-5 animated zoomIn"></div>
               <div class="item-inner">
                 <div class="item-title-row">
                   <div class="item-title">
-                    VideoStar++
-             </div> <a href="itms-services://?action=download-manifest&url= https://hamastore.netlify.app./install/videostar.plist"
+                    Videostar
+                  </div><a href="itms-services://?action=download-manifest&url=https:/aahmed-stor2.netlify.app/install/videostar.plist"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
-                  "icon f7-icons color-white">cloud_download</i></a>
+                  "icon f7-icons color-white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+                  </svg></i></a>
                 </div>
                 <div class="item-subtitle">
-                  Hacked
+                  GameHack
                 </div>
               </div>
             </div>
@@ -47,10 +49,10 @@
         </ul>
       </div>
       <div class="block-title">
-        About videostar++
+        About Videostar
       </div>
       <div class="block block-strong inset elevation-12">
-        <pre>Hacked - All Features Unlocked</pre>
+        <pre>Hacked</pre>
       </div>
       <div class="list inset elevation-12">
         <ul>
@@ -61,7 +63,7 @@
                   Version
                 </div>
                 <div class="item-after">
-                  v8.3.2
+                  v5.2
                 </div>
               </div>
             </div>
@@ -73,7 +75,7 @@
                   Size
                 </div>
                 <div class="item-after">
-                  97MB
+                  52MB
                 </div>
               </div>
             </div>
